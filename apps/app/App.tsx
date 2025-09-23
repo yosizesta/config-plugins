2a4dbacf7fb9121ccfb980deda60203f123b8c0f
Hassan Khan <hassan@expo.dev>
2025-09-23 20:32:15 +0300

    ver 54 with ffmpeg

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,3 +1,1 @@
-export { default } from "./src/detox/App";
-// export { default } from "./src/android-jsc-intl/App";
-// export { default } from "./src/react-native-adjust/App";
+export { default } from "./src/react-native-adjust/App";
